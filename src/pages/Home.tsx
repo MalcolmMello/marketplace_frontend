de5ade65322ca17a93c809e353c6268a7089f873
malcolm <malcolmmellolima@gmail.com>
2022-09-30 22:53:44 -0300

    homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,71 @@ export const Home = () => {
     return (
         <C.Home>
             <C.Header>
-                <div>
-                    Olá Petshop do Nau, <br/>
-                    <span>Bem vindo ao Portal do Parceiro!</span>
-                </div>
+                Olá Petshop do Nau, <br/>
+                <span>Bem vindo ao Portal do Parceiro!</span>
             </C.Header>
+            <C.AnalyticsOverview>
+                <h4>Estatísticas</h4>
+                <div className='data--area'>
+                    <div className='data--item'>
+                        <small>Pedidos de Hoje</small>
+                        <span><h2>0</h2> <small>pedidos</small></span>
+                    </div>
+                    <div className='data--item'>
+                        <small>Ticket Médio de Hoje</small>
+                        <span><h2>R$ 0,00</h2></span>
+                    </div>
+                    <div className='data--item'>
+                        <small>Vendas de Hoje</small>
+                        <span><h2>0</h2> <small>vendas</small></span>
+                    </div>
+                    <div className='data--item'>
+                        <small>Avaliação</small>
+                        <span><h2>5,0</h2></span>
+                    </div>
+                </div>
+            </C.AnalyticsOverview>
+            <C.SevenDaysPerformance>
+                <h4>Dados Gerais</h4>
+                <div className='perf--container'>
+                    <div className='perf--item'>
+                        <div className='title'>
+                            <h2>Vendas</h2>
+                            <small>Últimos 7 dias</small>
+                        </div>
+                        <span>
+                            <h2>0</h2>
+                        </span>
+                    </div>
+                    <div className='perf--item'>
+                        <div className='title'>
+                            <h2>Pedidos</h2>
+                            <small>Últimos 7 dias</small>
+                        </div>
+                        <span>
+                            <h2>0</h2>
+                        </span>
+                    </div>
+                    <div className='perf--item'>
+                        <div className='title'>
+                            <h2>Ticket Médio</h2>
+                            <small>Últimos 7 dias</small>
+                        </div>
+                        <span>
+                            <h2>R$ 0,00</h2>
+                        </span>
+                    </div>
+                    <div className='perf--item'>
+                        <div className='title'>
+                            <h2>Clientes</h2>
+                            <small>Últimos 7 dias</small>
+                        </div>
+                        <span>
+                            <h2>0</h2>
+                        </span>
+                    </div>
+                </div>
+            </C.SevenDaysPerformance>
         </C.Home>
     )
 };
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -2,12 +2,69 @@ import styled from "styled-components";
 
 export const Home = styled.main`
     width: 85vw;
-    padding: 10px 30px
-`
+    padding: 10px 60px 0px 40px;
+    background-color: rgb(244,244,244)
+`;
 
 export const Header = styled.header`
     span {
         color: #949494;
         font-size: 12px;
     }
+`;
+
+export const AnalyticsOverview = styled.section`
+    margin-top: 40px;
+    .data--area {
+        display: flex;
+        justify-content: space-between;
+        .data--item {
+            cursor: pointer;
+            padding: 25px 10px;
+            border-radius: 15px;
+            min-width: 200px;
+            background-color: #fff;
+            box-shadow: 0px 0px 30px rgb(235,235,235);
+            span {
+                display: flex;
+                align-items: center;
+                margin-top: 15px;
+                h2 {
+                    margin: 0px 10px 0px 0px
+                };
+                small {
+                    margin-top: 9px
+                }
+            }
+        }
+    }
 `
+
+export const SevenDaysPerformance = styled.section`
+    margin-top: 40px;
+    padding: 5px 15px;
+    border-radius: 15px;
+    background-color: #fff;
+    box-shadow: 0px 0px 30px rgb(235,235,235);
+    .perf--container {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+    };
+    .perf--item {
+        .title {
+            display: flex;
+            align-items: center;
+            h2 {
+                margin: 0 10px 7px 0
+            };
+            small {
+                font-size: 11px;
+            };
+            margin-bottom: 10px;
+        }
+        span h2 {
+            margin: 0
+        }
+        margin-bottom: 50px;
+    }    
+`;
