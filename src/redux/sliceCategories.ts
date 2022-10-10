da52243a03946b8c51a1f4d9fe2d62070f193041
malcolm <malcolmmellolima@gmail.com>
2022-10-10 13:08:59 -0300

    removing folder

--- src/redux/sliceCategories.ts
+++ src/redux/sliceCategories.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const baseURL = 'http://localhost:5000';
 
-const headers = { 'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJ1cmd1ZXJraW5nQGdtYWlsLmNvbSIsImlkIjoiYzZkOTI5ZTQtZWRiNy00ODZlLTk2MjMtOGZjN2E1YTBlZmVlIiwiaWF0IjoxNjY1MzM0NDQzLCJleHAiOjE2NjUzMzgwNDN9.XrdWNWmVUx85JgUgcDGqacLku6SgEyLwm1YPftI_BgY' };
+const headers = { 'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJ1cmd1ZXJraW5nQGdtYWlsLmNvbSIsImlkIjoiYzZkOTI5ZTQtZWRiNy00ODZlLTk2MjMtOGZjN2E1YTBlZmVlIiwiaWF0IjoxNjY1NDE3ODQwLCJleHAiOjE2NjU0MjE0NDB9.bF5g8RcItYPgc7A-jVSiRUPGwnx_5O6fq3dTMBan1Dw' };
 
 interface Response {
     id: string,
