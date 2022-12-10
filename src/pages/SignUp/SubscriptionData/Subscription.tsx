b06e3c2f5ef4b211a361ed3a8d44949ce38191d4
malcolm <malcolmmellolima@gmail.com>
2022-12-10 20:38:41 -0300

    create customer and subsctiption with stripe

--- src/pages/SignUp/SubscriptionData/Subscription.tsx
+++ src/pages/SignUp/SubscriptionData/Subscription.tsx
@@ -1,5 +1,5 @@
 import * as C from './styles';
-import { CardElement, Elements, useStripe, useElements, PaymentElement } from '@stripe/react-stripe-js';
+import { Elements, useStripe, useElements, PaymentElement } from '@stripe/react-stripe-js';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { loadStripe } from '@stripe/stripe-js';
@@ -10,7 +10,7 @@ const stripePromise = loadStripe(
 
 export const Subscription = () => {
     const stripe = useStripe();
-    const element = useElements();
+    const elements = useElements();
     const [clientSecret, setClientSecret] = useState("");
 
     useEffect(() => {
@@ -20,24 +20,43 @@ export const Subscription = () => {
     const getClientSecret = async () => {
         const { data } = await axios.get('http://localhost:5000/companies/secret');
         setClientSecret(data.clientSecret);
-    }; 
+    };
+
+    const createSubscription = async () => {
+        
+    }
+
     return (
         <C.Subscription>
             <div className='container'>
-                <div className='card--area'>
-                {clientSecret !== '' &&
-                    <Elements stripe={stripePromise} options={{clientSecret: clientSecret}}>
-                        <PaymentElement />
-                    </Elements> 
-                }
-                </div>
-                <div>
-                    <div>
-
-                    </div>
-                    <button>
-
-                    </button>
+                <h1>Parceiro Windpet</h1>
+                <div className='content'>
+                    {clientSecret !== '' &&
+                        <>
+                            <div className='card--area'>
+                                <Elements stripe={stripePromise} options={{clientSecret: clientSecret}}>
+                                    <PaymentElement />
+                                </Elements> 
+                            </div>
+                            <div className='description'>
+                                <div>
+                                    <h1>Plano Mensal</h1>
+                                    <div className='text'>
+                                        Torne-se um parceiro Windpet, tenha acesso a uma plataforma personalizada
+                                        para gerenciar pedidos, movimentação financeira e os produtos que colocar a venda.
+                                        Além de aumentar o seu número de vendas expondo seu Petshop em um aplicativo focado no mercado Pet.
+                                    </div>
+                                </div>
+                                <div className='total'>
+                                    <h2>Total</h2>
+                                    <h2>R$ 100,00</h2>
+                                </div>
+                                <button onClick={createSubscription}>
+                                    Finalizar cadastro
+                                </button>
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
         </C.Subscription>
--- src/pages/SignUp/SubscriptionData/styles.ts
+++ src/pages/SignUp/SubscriptionData/styles.ts
@@ -3,11 +3,59 @@ import styled from "styled-components";
 export const Subscription = styled.main`
     .container {
         display: flex;
-        max-width: 800px;
+        flex-direction: column;
+        align-items: center;
+        max-width: 900px;
         margin: auto;
 
-        .card--area {
-            width: 50%;
+        h1 {
+            color: #FF175B;
+        }
+
+        .content {
+            display: flex;
+
+            .card--area {
+                width: 50%;
+            }
+            
+            .description {
+                width: 50%;
+                margin-left: 30px;
+
+                .total {
+                    display: flex;
+                    justify-content: space-between;
+                    h2 {
+                        margin: 10px 0px;
+                        font-weight: 600
+                    }
+                }
+                button {
+                    width: 100%;
+                    height: 42px;
+                    background-color: #FF175B;
+                    border: none;
+                    border-radius: 10px;
+                    color: #FFF;
+                    font-weight: bold;
+                    font-size: 17px;
+                    margin-top: 10px;
+                    cursor: pointer
+                }
+
+                h1 {
+                    margin: 0px;
+                    font-size: 25px;
+                    color: #000;
+                    font-weight: 600
+                }
+
+                .text {
+                    font-size: 17px;
+                    margin-top: 8px
+                }
+            }
         }
     }
 `;
